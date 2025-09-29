0e5ffbbb685bcbaac46d75982c21067fbc8e463c
Jannes Müller <hypercookie@gmx.de>
2025-09-29 15:44:53 +0200

    fix(labels): Formatting

--- modules/labels/variables.tf
+++ modules/labels/variables.tf
@@ -1,14 +1,14 @@
 variable "namespace" {
-  type = string
+  type        = string
   description = "The namespace to build labels under"
 }
 
 variable "stages" {
-  type = set(string)
+  type        = set(string)
   description = "The different stages to build labels for"
 }
 
 variable "resources" {
-  type = set(string)
+  type        = set(string)
   description = "What resources will be present on each stage"
 }
