eb3df081710b0e5cd8c684d8ea430a6f4b5bb6ab
Jannes Müller <hypercookie@gmx.de>
2025-10-03 10:55:19 +0200

    fix(ips): Outputs

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -5,12 +5,12 @@ output "local_subnets" {
   value = local.local_only
 }
 
-output "https_firewall_id" {
-  value = try(hcloud_firewall.https[0].id, null)
+output "https_firewall" {
+  value = try(hcloud_firewall.https[0], null)
 }
-output "ssh_firewall_id" {
-  value = try(hcloud_firewall.ssh[0].id, null)
+output "ssh_firewall" {
+  value = try(hcloud_firewall.ssh[0], null)
 }
-output "ping_firewall_id" {
-  value = try(hcloud_firewall.ping[0].id, null)
+output "ping_firewall" {
+  value = try(hcloud_firewall.ping[0], null)
 }
--- modules/ips/outputs.tf
+++ modules/ips/outputs.tf
@@ -2,5 +2,5 @@ output "ipv4" {
   value = hcloud_primary_ip.ipv4
 }
 output "ipv6" {
-  value = try(hcloud_primary_ip.ipv6, null)
+  value = try(hcloud_primary_ip.ipv6[0], null)
 }
