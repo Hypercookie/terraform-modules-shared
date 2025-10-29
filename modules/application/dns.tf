836e9b3a345b22f4e8764a32e0addefe5b252309
Jannes Mueller <jannes.mueller@knime.com>
2025-10-29 19:10:53 +0100

    feat(Application): Create Application

--- modules/application/dns.tf
+++ modules/application/dns.tf
@@ -1,5 +1,5 @@
 resource "inwx_nameserver_record" "ipv4_entries" {
-  for_each = var.create_inwx_entries ? toset(local.fqdns) : toset([])
+  for_each = var.create_inwx_entries ? toset(var.subdomains) : toset([])
   content = var.endpoint_ipv4
   name = each.value
   domain  = var.base_domain
@@ -7,7 +7,7 @@ resource "inwx_nameserver_record" "ipv4_entries" {
 }
 
 resource "inwx_nameserver_record" "ipv6_entries" {
-  for_each = var.create_inwx_entries && var.ipv6_support ? toset(local.fqdns) : toset([])
+  for_each = var.create_inwx_entries && var.ipv6_support ? toset(var.subdomains) : toset([])
   content = var.endpoint_ipv6
   name = each.value
   domain  = var.base_domain
